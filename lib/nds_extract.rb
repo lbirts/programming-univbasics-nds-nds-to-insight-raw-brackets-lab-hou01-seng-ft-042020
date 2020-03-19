eee9c1d7a61fc1809ef07fff78664b4f9b5acef6
Lauren Birts <birtslauren@gmail.com>
2020-03-19 21:17:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,10 +3,11 @@ require 'directors_database'
 require 'pp'
 
 def directors_totals(nds)
-  pp nds[0]
+  pp nds[0][0]
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
-  # row_index = 0
+  
+  # director_index = 0
   # while row_index < nds.length do
     
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
