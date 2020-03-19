9007c5bdf83fcd398fb398a5d57f25c5d32daf67
Lauren Birts <birtslauren@gmail.com>
2020-03-19 21:15:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,8 +6,9 @@ def directors_totals(nds)
   pp nds
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
-  #
-  #
+  row_index = 0
+  while row_index < nds.length do
+    
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
