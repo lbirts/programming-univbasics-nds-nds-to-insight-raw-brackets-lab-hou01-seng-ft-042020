9c58dc816cbcac2c7edd75a5cfd85d377d59ad20
Lauren Birts <birtslauren@gmail.com>
2020-03-19 21:22:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,7 +3,7 @@ require 'directors_database'
 require 'pp'
 
 def directors_totals(nds)
-  pp nds[0]
+  pp nds
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   # director_index = 0
